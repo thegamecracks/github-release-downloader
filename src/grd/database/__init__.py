224003be66fc65dc907719fa03872eeb168bc9ec
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2023-03-14 12:03:07 -0400

    Setup database on database import

--- src/grd/database/__init__.py
+++ src/grd/database/__init__.py
@@ -1,14 +1,13 @@
+from pathlib import Path
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from .models import Base, Response, User
 from ..appdirs import dirs
 
 
-def setup_schema():
-    from .models import Base
-
-    Base.metadata.create_all(data_engine)
-
-
+Path(dirs.user_data_dir).mkdir(parents=True, exist_ok=True)
 data_engine = create_engine(f"sqlite+pysqlite:///{dirs.user_data_dir}/data.db")
 data_session = sessionmaker(data_engine)
+Base.metadata.create_all(data_engine)
