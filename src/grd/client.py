b5bb6066fe6065fac7b0191afc58fbc632bc8f60
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2023-03-14 00:38:14 -0400

    Add typehints to ReleaseRequester

--- src/grd/client.py
+++ src/grd/client.py
@@ -1,11 +1,14 @@
 import json
 import logging
 import textwrap
+from typing import Any, BinaryIO, Iterator
 
+import httpx
 from tqdm import tqdm
 
 
-def stream_progress(response):
+def stream_progress(response: httpx.Response) -> Iterator[bytes]:
+    """Yields bytes from a response while displaying a progress bar."""
     progress = tqdm(
         total=int(response.headers["Content-Length"]),
         unit="B",
@@ -19,28 +22,33 @@ def stream_progress(response):
 
 
 class ReleaseRequester:
+    """Handles making requests to GitHub."""
+
     JSON_HEADERS = {"Accept": "application/vnd.github+json"}
 
-    def __init__(self, client, owner, repo):
+    def __init__(self, client: httpx.Client, owner: str, repo: str):
         self.client = client
         self.owner = owner
         self.repo = repo
 
-    def get_release(self):
+    def get_release(self) -> dict:
+        """Gets the repository's latest release."""
         response = self.client.get(
             f"/repos/{self.owner}/{self.repo}/releases/latest",
             headers=self.JSON_HEADERS,
         )
         return self._as_json(response)
 
-    def get_release_assets(self, release_id):
+    def get_release_assets(self, release_id: int) -> list[dict]:
+        """Gets a list of assets for the given release ID."""
         response = self.client.get(
             f"/repos/{self.owner}/{self.repo}/releases/{release_id}/assets",
             headers=self.JSON_HEADERS,
         )
         return self._as_json(response)
 
-    def download_asset_to(self, file, asset_id):
+    def download_asset_to(self, file: BinaryIO, asset_id: int) -> None:
+        """Downloads an asset to the given file."""
         url = f"/repos/{self.owner}/{self.repo}/releases/assets/{asset_id}"
         headers = {"Accept": "application/octet-stream"}
 
@@ -55,7 +63,7 @@ class ReleaseRequester:
             for data in stream_progress(response):
                 file.write(data)
 
-    def _as_json(self, response):
+    def _as_json(self, response: httpx.Response) -> Any:
         data = response.json()
         logging.debug(
             "\n%s",
