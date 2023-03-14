ab2fae552233a47f81a36d211caea43d852321c7
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2023-03-14 14:02:30 -0400

    Apply improvements to sqlite3 connections

--- src/grd/database/__init__.py
+++ src/grd/database/__init__.py
@@ -1,10 +1,26 @@
+import sqlite3
 from pathlib import Path
 
-from sqlalchemy import create_engine
+from sqlalchemy import Connection, Engine, create_engine, event
 from sqlalchemy.orm import sessionmaker
 
 from .models import Base, Response, User
 from ..appdirs import dirs
+
+
+# Apply various improvements to sqlite3 connections
+# https://docs.sqlalchemy.org/en/20/dialects/sqlite.html#foreign-key-support
+# https://docs.sqlalchemy.org/en/20/dialects/sqlite.html#serializable-isolation-savepoints-transactional-ddl
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(conn: sqlite3.Connection, record):
+    conn.isolation_level = None
+    conn.execute("PRAGMA foreign_keys = on")
+    conn.execute("PRAGMA journal_mode = wal")
+
+
+@event.listens_for(Engine, "begin")
+def do_begin(conn: Connection):
+    conn.exec_driver_sql("BEGIN")
 
 
 Path(dirs.user_data_dir).mkdir(parents=True, exist_ok=True)
