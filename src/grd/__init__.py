fa2c2fbb4329eb7593665208b96e6117be9af883
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2023-03-19 17:24:47 -0400

    Add User-Agent when making requests

--- src/grd/__init__.py
+++ src/grd/__init__.py
@@ -1,1 +1,3 @@
+__qualname__ = "github-release-downloader"
 __version__ = "0.1.0"
+__url__ = "https://github.com/thegamecracks/github-release-downloader"
--- src/grd/client/http.py
+++ src/grd/client/http.py
@@ -1,10 +1,20 @@
+import sys
 from typing import Iterator
 
 import httpx
 from tqdm import tqdm
 
+from .. import __qualname__, __version__, __url__
+
 BASE = "https://api.github.com"
-HEADERS = {"X-GitHub-Api-Version": "2022-11-28"}
+HEADERS = {
+    "User-Agent": (
+        f"{__qualname__}/{__version__} ({__url__}) "
+        + "Python/{0.major}.{0.minor} ".format(sys.version_info)
+        + f"httpx/{httpx.__version__}"
+    ),
+    "X-GitHub-Api-Version": "2022-11-28",
+}
 
 
 def create_client(*, token: str | None = None) -> httpx.Client:
